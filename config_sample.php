1fe7b0fe63bac67760ed5da7a07f36c37c753450
ulrischa <ulrischa@users.noreply.github.com>
2025-03-24 21:28:26 +0100

    Create config_sample.php

---
+++ config_sample.php
@@ -0,0 +1,17 @@
+<?php
+namespace ulrischa/rssfeedmanager;
+
+return [
+    'feeds' => [
+        'feed1' => [
+            'api_key'     => 'your_api_key_1',
+            'max_entries' => 10,
+        ],
+        'feed2' => [
+            'api_key'     => 'your_api_key_2',
+            'max_entries' => 20,
+        ],
+        // Additional feeds can be added here...
+    ],
+];
+?>
